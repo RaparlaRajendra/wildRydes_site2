a2c0fbc359277aa752ccdc5166988126295221a2
RaparlaRajendra <160262152+RaparlaRajendra@users.noreply.github.com>
2025-04-15 13:48:04 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_SM1MoAp45', //ap-south-1_SM1MoAp45
+        userPoolClientId: '309h2b9vuhq8g221bhjf7u940s', // ap-south-1_SM1MoAp45
+        region: 'ap-south-1' // ap-south-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
