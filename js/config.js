824ed6dc56d284a5b03a6571105ffdd033e92412
RaparlaRajendra <160262152+RaparlaRajendra@users.noreply.github.com>
2025-04-24 14:34:19 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_pTdvS2nla', //ap-south-1_SM1MoAp45
-        userPoolClientId: '2qhslh003ud5kvevj0er1b73fe', // ap-south-1_SM1MoAp45
+        userPoolId: 'ap-south-1_ToWbmJ2C5', //ap-south-1_SM1MoAp45
+        userPoolClientId: '4f91sien0kq984a7eaamltgktk', // ap-south-1_SM1MoAp45
         region: 'ap-south-1' // ap-south-1
     },
     api: {
-        invokeUrl: 'https://im2jmuasa6.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
