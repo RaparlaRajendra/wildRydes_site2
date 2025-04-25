b1569d24a8453ac7c65bbf4a88645decb9feb2a3
RaparlaRajendra <160262152+RaparlaRajendra@users.noreply.github.com>
2025-04-25 20:56:48 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_ToWbmJ2C5', //ap-south-1_SM1MoAp45
-        userPoolClientId: '4f91sien0kq984a7eaamltgktk', // ap-south-1_SM1MoAp45
+        userPoolId: 'ap-south-1_GS9ONtpbh', //ap-south-1_SM1MoAp45
+        userPoolClientId: '3l724ht5j0gsa2v5cueu01rcjc', // ap-south-1_SM1MoAp45
         region: 'ap-south-1' // ap-south-1
     },
     api: {
